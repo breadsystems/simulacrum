740b67d76707a12e43f3793c94ee62269640441e
Coby Tamayo <acobster@gmail.com>
2021-03-31 11:42:15 -0700

    Refactor upload for cleanliness

--- public/api/index.php
+++ public/api/index.php
@@ -1,5 +1,9 @@
 <?php
 
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Simulacrum\Upload;
+
 $keyFile = __DIR__ . '/../../api.key';
 $hash    = is_readable($keyFile) ? file_get_contents($keyFile) : '';
 if (!$hash) {
@@ -23,69 +27,19 @@ if (!is_writeable(IMAGES_ROOT) || !is_dir(IMAGES_ROOT)) {
   exit;
 }
 
-// TODO handle DELETE
-
-$img = file_get_contents('php://input');
-if (!$img) {
-  header('HTTP/1.1 400 Bad Request');
-  echo "No image uploaded.\n";
-  exit;
-}
-
-$finfo = new finfo(FILEINFO_MIME_TYPE);
-$mime  = $finfo->buffer($img);
-if (!in_array($mime, [
-  'image/png',
-  'image/jpeg',
-  'image/gif',
-  'image/webp',
-  'image/tiff',
-])) {
-  header('HTTP/1.1 400 Bad Request');
-  echo "Invalid MIME type.\n";
-  exit;
-}
-
-if (empty($_GET['file'])) {
-  header('HTTP/1.1 400 Bad Request');
-  echo "You must specify a file name to save to, i.e. ?file=...\n";
-  exit;
-}
-
-// Filter out blank dirs as a result of duplicate or leading/trailing slashes.
-$segments = array_filter(explode('/', $_GET['file']));
-
-if (count($segments) !== 2) {
-  header('HTTP/1.1 400 Bad Request');
-  echo "file path must be exactly two levels deep (dir/file.ext).\n";
-  exit;
-}
-
-[$dir, $file] = $segments;
-
-if (strpos($dir, '.') !== false) {
-  header('HTTP/1.1 400 Bad Request');
-  echo "Directory name cannot contain dots (".").\n";
-  exit;
-}
-
-$path = implode(DIRECTORY_SEPARATOR, [IMAGES_ROOT, $dir, $file]);
-
-$newDir = false;
-if (!is_dir(dirname($path))) {
-  mkdir(dirname($path));
-  $newDir = true;
-}
-
-file_put_contents($path, $img);
-
-[$width, $height] = getimagesize($path);
-
-header('Content-Type: application/json');
-echo json_encode([
-  'success'    => true,
-  'path'       => implode(DIRECTORY_SEPARATOR, [$dir, $file]),
-  'mime_type'  => $mime,
-  'dimensions' => [$width, $height],
-  'new_dir'    => $newDir,
+$res = Upload\handle([
+  'http_method' => $_SERVER['REQUEST_METHOD'],
+  'image_data'  => file_get_contents('php://input'),
 ]);
+
+if ($res['status'] === 200) {
+  header('Content-Type: application/json');
+} else {
+  header([
+    400 => 'HTTP/1.1 400 Bad Request',
+    404 => 'HTTP/1.1 404 Not Found',
+    500 => 'HTTP/1.1 500 Internal Server Error',
+  ][$res['status'] ?? 500]);
+}
+
+echo json_encode($res['body']);
---
+++ src/Upload.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Simulacrum\Upload;
+
+use finfo;
+
+define('API_METHODS', [
+  'PUT'    => upload::class,
+  'DELETE' => delete::class,
+]);
+
+function handle(array $req) : array {
+  $handler = API_METHODS[$_SERVER['REQUEST_METHOD']] ?? null;
+  if (!$handler) {
+    return [
+      'status' => 400,
+      'body'   => 'NOPE',
+    ];
+  }
+
+  return $handler($req);
+}
+
+function delete(array $req) : array {
+  // TODO
+}
+
+function upload(array $req) : array {
+  $img = file_get_contents('php://input');
+  if (!$img) {
+    return [
+      'status' => 400,
+      'body'   => [
+        'success' => false,
+        'error'   => 'No file uploaded',
+      ],
+    ];
+  }
+
+  $finfo = new finfo(FILEINFO_MIME_TYPE);
+  $mime  = $finfo->buffer($img);
+  if (!in_array($mime, [
+    'image/png',
+    'image/jpeg',
+    'image/gif',
+    'image/webp',
+    'image/tiff',
+  ])) {
+    return [
+      'status'    => 400,
+      'body'      => [
+        'success' => false,
+        'error'   => 'Invalid MIME type.',
+      ],
+    ];
+  }
+
+  if (empty($_GET['file'])) {
+    return [
+      'status'    => 400,
+      'body'      => [
+        'success' => false,
+        'error'   => 'You must specify a filename, i.e. ?file=dir/file.ext',
+      ],
+    ];
+  }
+
+  // Filter out blank dirs as a result of duplicate or leading/trailing slashes.
+  $segments = array_filter(explode('/', $_GET['file']));
+
+  if (count($segments) !== 2) {
+    return [
+      'status'    => 400,
+      'body'      => [
+        'success' => false,
+        'error'   => 'file path must be exactly two levels deep (dir/file.ext)',
+      ],
+    ];
+  }
+
+  [$dir, $file] = $segments;
+
+  if (strpos($dir, '.') !== false) {
+    return [
+      'status'    => 400,
+      'body'      => [
+        'success' => false,
+        'error'   => 'Directory name cannot contain dots (".")',
+      ],
+    ];
+  }
+
+  $path = implode(DIRECTORY_SEPARATOR, [IMAGES_ROOT, $dir, $file]);
+
+  $newDir = false;
+  if (!is_dir(dirname($path))) {
+    mkdir(dirname($path));
+    $newDir = true;
+  }
+
+  $bytes = file_put_contents($path, $img);
+
+  if (!$bytes) {
+    return [
+      'status'    => 500,
+      'body'      => [
+        'success' => false,
+        'error'   => 'No bytes written! Try again?',
+      ],
+    ];
+  }
+
+  [$width, $height] = getimagesize($path);
+
+  return [
+    'status'      => 200,
+    'body'        => [
+      'success'   => true,
+      'path'      => implode(DIRECTORY_SEPARATOR, [$dir, $file]),
+      'mime_type' => $mime,
+      'width'     => $width,
+      'height'    => $height,
+      'bytes'     => $bytes,
+      'new_dir'   => $newDir,
+    ],
+  ];
+}
