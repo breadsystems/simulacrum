e325c6256496b3fd711f6c8b86ac04a91cd4b882
Coby Tamayo <coby@tamayo.email>
2026-01-30 20:00:54 -0800

    use flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Simulacrum dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells."${system}".default = let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in pkgs.mkShell {
+        packages = with pkgs; [
+          php83
+        ];
+      };
+    };
+}
