c3fe404c8cc783465dff344368c69781fc750fdf
Coby Tamayo <coby@tamayo.email>
2026-01-31 20:53:35 -0800

    Image\handle

--- public/index.php
+++ public/index.php
@@ -2,10 +2,8 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Gumlet\ImageResizeException;
-
 use Simulacrum\Api;
-use Simulacrum\ParseError;
+use Simulacrum\Image;
 
 $path = $_SERVER['PATH_INFO'] ?? '';
 if (substr($path, 0, 4) === '/api') {
@@ -34,38 +32,7 @@ if (substr($path, 0, 4) === '/api') {
   exit;
 }
 
-// let the frontend handle favicons
-if ($_SERVER['REQUEST_URI'] === '/favicon.ico') {
-  header('HTTP/1.1 404 Not Found');
-  exit;
-}
-
-define('MAX_AGE', intval(getenv('MAX_AGE') ?: 10 * 365 * 24 * 3600));
-
-try {
-  $chain = Simulacrum\parse_uri($_SERVER['REQUEST_URI']);
-} catch (ParseError $e) {
-  header('HTTP/1.1 400 Bad Request');
-  header('X-Error: ' . $e->getMessage());
-  exit;
-}
-
-$since = $_SERVER['HTTP_IF_MODIFIED_SINCE'] ?? 0;
-$staleAt = $since ? strtotime($since) : 0;
-$lastModified = $chain['stat']['mtime'] ?? PHP_INT_MAX;
-if ($staleAt > $lastModified) {
-  header('HTTP/1.1 304 Not Modified');
-  exit;
-}
-
-try {
-  $image = Simulacrum\execute($chain);
-  header('Last-Modified: ' . date('D, d M Y h:m:s \G\M\T', $lastModified));
-  header('Cache-Control: public, max-age=' . MAX_AGE . ', immutable');
-} catch (ImageResizeException $e) {
-  header('HTTP/1.1 404 Not Found');
-  header('X-Error: ' . $e->getMessage());
-  exit;
-}
-
-echo $image->output($chain['type']);
+echo Image\handle([
+  'uri'               => $_SERVER['REQUEST_URI'],
+  'if-modified-since' => $_SERVER['HTTP_IF_MODIFIED_SINCE'] ?? 0,
+]);
---
+++ src/Image.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Simulacrum\Image;
+
+use Gumlet\ImageResizeException;
+
+use Simulacrum;
+use Simulacrum\ParseError;
+
+define('MAX_AGE', intval(getenv('MAX_AGE') ?: 10 * 365 * 24 * 3600));
+
+function handle(array $req) {
+  try {
+    $chain = Simulacrum\parse_uri($req['uri']);
+  } catch (ParseError $e) {
+    header('HTTP/1.1 400 Bad Request');
+    header('X-Error: ' . $e->getMessage());
+    exit;
+  }
+
+  $staleAt = $req['if-modified-since'] ? strtotime($req['if-modified-since']) : 0;
+  $lastModified = $chain['stat']['mtime'] ?? PHP_INT_MAX;
+  if ($staleAt > $lastModified) {
+    header('HTTP/1.1 304 Not Modified');
+    exit;
+  }
+
+  try {
+    $image = Simulacrum\execute($chain);
+    header('Last-Modified: ' . date('D, d M Y h:m:s \G\M\T', $lastModified));
+    header('Cache-Control: public, max-age=' . MAX_AGE . ', immutable');
+  } catch (ImageResizeException $e) {
+    header('HTTP/1.1 404 Not Found');
+    header('X-Error: ' . $e->getMessage());
+    exit;
+  }
+
+  return $image->output($chain['type']);
+}
